436b55f09bcea27a23f24a17c6b97b5bc1307348
JoseMgil <35809460+JoseMgil@users.noreply.github.com>
2018-03-08 16:34:59 +0100

    Update optional_op.scala

--- optional_op.scala
+++ optional_op.scala
@@ -1,1 +1,42 @@
-## Optional Options
+/*
+  Examples of Optional values and Either
+  By Mgil
+*/
+
+/* 
+* Optional return value
+* - Option[A] reprepent a optional Return value as A Type
+* - If there is no value, ret: None
+* - If there is a value, ret: Some(x) --> Note that x could be a operation
+*/
+def sqrtOption(x: Double): Option[Double] = {
+if (x < 0) None
+else Some(x * x)
+}
+
+/*
+* Either return value
+* - Either[A, B]: You can choose between A or B type to return in your function
+* - For the A type, uses Left(x)
+* - For the B type, uses Right(x)
+*/
+def sqrtEither(x: Double): Either[Double, String] = {
+if (x < 0) Right("x must be positive")
+else 
+   Left(x * x)
+}
+
+// Test cases
+println(sqrtOption(2))
+println(sqrtOption(-2))
+println(sqrtEither(2))
+println(sqrtEither(-2))
+
+/* Results: 
+Some(4)
+None
+Left(4)
+Right(x must be positive)
+*/
+
+  
