07166fd0f1821472e5278602e4debd66680c5f9a
JoseMgil <35809460+JoseMgil@users.noreply.github.com>
2018-04-09 18:21:00 +0200

    Create abstract.scala

---
+++ abstract.scala
@@ -0,0 +1,30 @@
+
+/*
+  Abstract class with implementations
+  No instances of an abstract class can be created with the operator "new"
+*/
+
+abstract class IntSet {
+  def incl(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
+
+class Empty extends IntSet {
+  def contains(x: Int): Boolean = false
+  def incl(x: Int): IntSet = new NoEmpty(x, new Empty, new Empty)
+
+}
+
+class NoEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
+  
+  def contains(x: Int): Boolean = 
+    if(x < elem) left.contains(x)
+    else if(x > elem) right.contains(x)
+    else true
+  
+  def incl(x: Int): IntSet = 
+    if(x < elem) new NoEmpty(elem, left.incl(x), right)
+    else if(x > elem) new NoEmpty(elem, left, right.incl(x))
+    else this
+    
+}
