e95782640ccf66c5d5349462f5c5be7d329088e6
JoseMgil <35809460+JoseMgil@users.noreply.github.com>
2018-09-11 14:15:15 +0200

    Update class RealizaImputacion

--- imputaciones.scala
+++ imputaciones.scala
@@ -1,7 +1,8 @@
+// Start writing your ScalaFiddle code here
 val cod1 = "Z/0912"
 val cod2 = "Z/1094"
 val cod3 = "Z/1029"
-val h1 = 3 
+val h1 = 10
 val h2 = 46
 
 class Imputacion(v_codigo: String, v_horas: Int) {
@@ -27,10 +28,35 @@ class Imputacion(v_codigo: String, v_horas: Int) {
   
 }
 
-val imp1 = 
-new Imputacion(cod1,h2)
 
+class RealizaImputacion(v_nombre: String, v_totalImputar: Int, v_listaImp: List[Imputacion]) {
+  
+  var nombre = v_nombre
+  var listaImp = v_listaImp
+  var totalImputar = v_totalImputar
+  
+  def imputacionCorrecta(): Boolean = {
+    //TODO: Implementar 
+    if(sumaImputaciones(listaImp) == totalImputar) true
+    else false
+  }
+  
+  def sumaImputaciones(list: List[Imputacion]): Int = {
+    list match{
+      case List() => 0 
+      case x :: xs => x.horas + sumaImputaciones(xs)
+    }
+  }
+  
+}
+
+
+val imp1 = new Imputacion(cod1,h1)
+val imp2 = new Imputacion(cod2,h2)
 
 println("Total días: " + imp1.totalDias(8))
 println("La imputación es redonda: " + imp1.esImputacionRedonda(8))
 println(imp1 + "-> " + imp1.totalDias(8) + " dias y " + imp1.restoHoras(8) + " horas")
+
+val imputar1 = new RealizaImputacion("Jose", 56, List(imp1, imp2))
+println(imputar1.imputacionCorrecta())
